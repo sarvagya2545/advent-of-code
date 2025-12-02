8624ca95479dc6f33ce14b432621638ebcab8cc5
Sarvagya Sharma <sarvagya2545@gmail.com>
2025-12-02 10:15:56 +0530

    Initial commit 2025

---
+++ 2025/1.py
@@ -0,0 +1,32 @@
+
+# PART 1
+with open("input.txt", 'r') as file:
+    cur = 50
+    cnt = 0
+    for line in file.readlines():
+        dir = -1 if line[0] == 'L' else 1
+        num = int(line[1:]) * dir
+        cur = (cur + num + 100) % 100
+        if cur == 0:
+            cnt += 1
+
+    print(cnt)
+
+# PART 2
+with open("input.txt", 'r') as file:
+    cur = 50
+    cnt = 0
+    for line in file.readlines():
+        num = int(line[1:])
+        if num >= 100:
+            cnt += num // 100
+            num = num % 100
+        dir = -1 if line[0] == 'L' else 1
+        num *= dir
+        nxt = cur + num
+        # print(cur, nxt)
+        if (nxt <= 0 and cur - 1 >= 0) or (cur + 1 <= 100 and nxt >= 100):
+            cnt += 1
+        cur = (nxt + 100) % 100
+
+    print(cnt)
