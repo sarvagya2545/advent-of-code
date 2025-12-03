f647fa6d3a6227fd12f66837ee731365d09536a3
Sarvagya Sharma <sarvagya2545@gmail.com>
2025-12-03 19:23:53 +0530

    day 3 part 2 done

--- 2025/3.py
+++ 2025/3.py
@@ -1,13 +1,23 @@
-# PART 1
+def get_joltage(bank, k):
+    n = len(bank)
+    res = ''
+    st = 0
+    for i in range(k, 0, -1):
+        lst = list((chr, -(st + i)) for i, chr in enumerate(bank[st:n-i+1]))
+        c, idx = max(lst)
+        res += c
+        st = -idx + 1
+    return int(res)
+        
+# PART 2
 with open("input.txt") as file:
     tot = 0
     for bank in file.readlines():
-        mx = 0
-        cur_mx = ord(bank[-1]) - ord('0')
-        for i in range(len(bank) - 2, -1, -1):
-            num = ord(bank[i]) - ord('0')
-            mx = max(mx, num * 10 + cur_mx)
-            cur_mx = max(cur_mx, num)
-        tot += mx
+        bank = bank.rstrip('\n')
+        # PART 1
+        # tot += get_joltage(bank, 2)
+
+        # PART 2
+        tot += get_joltage(bank, 12)
     print(tot)
         
