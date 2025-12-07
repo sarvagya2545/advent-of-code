fe877205e79c838ba2021f837e8aeb61a574f953
Sarvagya Sharma <sarvagya2545@gmail.com>
2025-12-07 11:25:41 +0530

    Day 7 part 2

--- 2025/7.py
+++ 2025/7.py
@@ -8,14 +8,28 @@ for idx in range(0, len(grid[0])):
     if grid[0][idx] == 'S':
         beams.add(idx)
 
+timelines_count = [0] * len(grid[0])
+for idx in beams:
+    timelines_count[idx] = 1
+
 split_count = 0
 for row in range(1, len(grid)):
     splitters = [idx for idx in range(0, len(grid[row])) if grid[row][idx] == '^']
+    new_timelines_count = timelines_count
     for split in splitters:
         if split in beams:
             split_count += 1
             beams.remove(split)
             beams.add(split + 1)
             beams.add(split - 1)
+            x = new_timelines_count[split]
+            new_timelines_count[split] = 0
+            new_timelines_count[split - 1] += x
+            new_timelines_count[split + 1] += x
+    timelines_count = new_timelines_count
 
+# PART 1
 print(split_count)
+
+# PART 2
+print(sum(timelines_count))
