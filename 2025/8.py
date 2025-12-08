8cf9127b9a00e8b3649117ea55095cbd2cea86e2
Sarvagya Sharma <sarvagya2545@gmail.com>
2025-12-08 21:37:49 +0530

    Day 8 part 2

--- 2025/8.py
+++ 2025/8.py
@@ -10,6 +10,8 @@ with open("input.txt", 'r') as file:
         points.append((int(x), int(y), int(z)))
 
 n = len(points)
+
+# PART 1
 min_heap = []
 dist = lambda i, j: sum((points[i][dim] - points[j][dim]) ** 2 for dim in range(3))
 
@@ -49,3 +51,49 @@ for x in sorted(cnts, reverse=True)[:3]:
     ans *= x
 
 print(ans)
+
+# PART 2
+class DSU:
+    def __init__(self, n):
+        self.par = [None] * n
+        self.wt = [1] * n
+        for i in range(0, n):
+            self.par[i] = i
+    
+    def join(self, u, v):
+        u = self.root(u)
+        v = self.root(v)
+        
+        if u == v:
+            return
+
+        if self.wt[u] > self.wt[v]:
+            tmp = u
+            u = v
+            v = tmp
+        
+        self.par[u] = v
+
+    def root(self, x):
+        if self.par[x] == x:
+            return x
+        return self.root(self.par[x])
+
+    def is_connected(self, u, v):
+        return self.root(u) == self.root(v)
+
+
+distances: List[Tuple[int, int, int]] = []
+for i in range(0, n):
+    for j in range(i + 1, n):
+        distances.append((dist(i, j), i, j))
+
+dsu = DSU(n)
+connected_components_count = n
+for _, i, j in sorted(distances):
+    if not dsu.is_connected(i, j):
+        dsu.join(i, j)
+        connected_components_count -= 1
+    if connected_components_count == 1:
+        print(points[i][0] * points[j][0])
+        break
